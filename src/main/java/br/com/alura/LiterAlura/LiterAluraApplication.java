a17162ae4cb9da87cc8a5eaf78ecafb29c526637
Abner Vital <avgchaves@gmail.com>
2025-06-23 17:16:45 -0300

    refactor: injeta repositórios em LiterAluraApplication e adapta construtor de Principal

--- src/main/java/br/com/alura/LiterAlura/LiterAluraApplication.java
+++ src/main/java/br/com/alura/LiterAlura/LiterAluraApplication.java
@@ -1,13 +1,20 @@
 package br.com.alura.LiterAlura;
 
 import br.com.alura.LiterAlura.Principal.Principal;
+import br.com.alura.LiterAlura.repository.AutorRepository;
+import br.com.alura.LiterAlura.repository.LivroRepository;
 import br.com.alura.LiterAlura.service.ConsumoAPI;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LiterAluraApplication implements CommandLineRunner {
+	@Autowired
+	private LivroRepository livroRepository;
+	@Autowired
+	private AutorRepository autorRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(LiterAluraApplication.class, args);
@@ -15,7 +22,7 @@ public class LiterAluraApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		Principal principal = new Principal();
+		Principal principal = new Principal(livroRepository, autorRepository);
 		principal.menu();
 	}
 }
