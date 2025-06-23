085abddf23368f4a18e4de197841400e7db626bc
Abner Vital <avgchaves@gmail.com>
2025-06-23 00:57:07 -0300

    feat: cria DTOs DadosLivro, DadosLivros e DadosAutor com anotações JsonAlias

---
+++ src/main/java/br/com/alura/LiterAlura/model/DadosLivro.java
@@ -0,0 +1,14 @@
+package br.com.alura.LiterAlura.model;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record DadosLivro(@JsonAlias("title") String titulo,
+                         @JsonAlias("authors") List<DadosAutor> autor,
+                         @JsonAlias("download_count") Double totalDownloads,
+                         @JsonAlias("languages")ArrayList<String> idioma) {
+}
