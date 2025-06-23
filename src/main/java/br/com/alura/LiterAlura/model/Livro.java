1d4baee4563159c07a9c4094a8c664326e977c58
Abner Vital <avgchaves@gmail.com>
2025-06-23 17:14:57 -0300

    feat(model): mapeia entidade Livro com JPA e vínculo com Autor

--- src/main/java/br/com/alura/LiterAlura/model/Livro.java
+++ src/main/java/br/com/alura/LiterAlura/model/Livro.java
@@ -1,21 +1,38 @@
 package br.com.alura.LiterAlura.model;
 
-import com.fasterxml.jackson.annotation.JsonAlias;
+import jakarta.persistence.*;
 
-import java.util.ArrayList;
-import java.util.List;
-
+@Entity
+@Table(name = "livros")
 public class Livro {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
     private String titulo;
-    private String autor;
+
+    @ManyToOne
+    private Autor autor;
+
     private Double totalDownloads;
     private String idioma;
 
+    public Livro(){}
+
     public Livro(DadosLivros dados) {
         this.titulo = dados.livros().getFirst().titulo();
-        this.autor = dados.livros().getFirst().autor().getFirst().nome();
         this.totalDownloads = dados.livros().getFirst().totalDownloads();
         this.idioma = dados.livros().getFirst().idioma().getFirst();
+    }
+
+    public Autor getAutor() {
+        return autor;
+    }
+
+    public void setAutor(Autor autor) {
+        this.autor = autor;
     }
 
     public String getTitulo() {
@@ -35,14 +52,6 @@ public class Livro {
         this.totalDownloads = totalDownloads;
     }
 
-    public String getAutor() {
-        return autor;
-    }
-
-    public void setAutor(String autor) {
-        this.autor = autor;
-    }
-
     public String getIdioma() {
         return idioma;
     }
@@ -51,11 +60,21 @@ public class Livro {
         this.idioma = idioma;
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     @Override
     public String toString() {
-        return "Título: " + titulo + " | " +
-                "Autor: " + autor + " | " +
-                "Total de Downloads: " + totalDownloads.intValue() + " | " +
-                "Idioma: " + idioma;
+        return  "---- LIVRO ----\n" +
+                "Título: " + titulo + "\n" +
+                "Autor: " + autor.getNome() + "\n" +
+                "Total de Downloads: " + totalDownloads.intValue() + "\n" +
+                "Idioma: " + idioma + "\n" +
+                "---------------";
     }
 }
