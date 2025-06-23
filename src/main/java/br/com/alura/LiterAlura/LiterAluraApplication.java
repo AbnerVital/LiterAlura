66161d0d65719820dcfcc9741b5b8b327b281474
Abner Vital <avgchaves@gmail.com>
2025-06-23 01:05:18 -0300

    feat: executa menu principal com CommandLineRunner

--- src/main/java/br/com/alura/LiterAlura/LiterAluraApplication.java
+++ src/main/java/br/com/alura/LiterAlura/LiterAluraApplication.java
@@ -1,13 +1,21 @@
 package br.com.alura.LiterAlura;
 
+import br.com.alura.LiterAlura.Principal.Principal;
+import br.com.alura.LiterAlura.service.ConsumoAPI;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LiterAluraApplication {
+public class LiterAluraApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LiterAluraApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Principal principal = new Principal();
+		principal.menu();
+	}
 }
