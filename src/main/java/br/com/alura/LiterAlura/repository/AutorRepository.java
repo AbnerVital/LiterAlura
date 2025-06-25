4b2f3be2f838ab5570cb095dd7a822d53550ca85
Abner Vital <avgchaves@gmail.com>
2025-06-25 11:44:39 -0300

    feat: adiciona query para listar autores vivos em um determinado ano

--- src/main/java/br/com/alura/LiterAlura/repository/AutorRepository.java
+++ src/main/java/br/com/alura/LiterAlura/repository/AutorRepository.java
@@ -2,9 +2,14 @@ package br.com.alura.LiterAlura.repository;
 
 import br.com.alura.LiterAlura.model.Autor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface AutorRepository extends JpaRepository<Autor, Long> {
     Optional<Autor> findBynome(String autor);
+
+    @Query("SELECT a FROM Autor a WHERE a.dtNascimento <= :anoBusca AND a.dtFalecimento IS NULL OR a.dtFalecimento > :anoBusca")
+    Optional<List<Autor>> listarAutoresVivos(int anoBusca);
 }
