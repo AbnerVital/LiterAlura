f34afa009378eed2fd7e7da76244d6ea9109b104
Abner Vital <avgchaves@gmail.com>
2025-06-25 11:43:52 -0300

    feat: adiciona opções de listagem por idioma e autores vivos no menu, com validações e persistência condicional

--- src/main/java/br/com/alura/LiterAlura/Principal/Principal.java
+++ src/main/java/br/com/alura/LiterAlura/Principal/Principal.java
@@ -8,6 +8,8 @@ import br.com.alura.LiterAlura.service.ConverteDados;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Optional;
 import java.util.Scanner;
 
@@ -25,51 +27,94 @@ public class Principal {
         this.autorRepository = autorRepository;
     }
 
-    public void menu(){
+    public void menu() {
 
         var opcao = -1;
         while (opcao != 0) {
-            var menu = """
-                    Escolha o número de sua opção:
-                    1 - Buscar livro pelo título
-                    2 - Listar livros registrados
-                    3 - Listar autores registrados
-                    4 - Listar autores vivos em um determinado ano
-                    5 - Listar livros de um determinado idioma
-                    
-                    0 - Sair
-                    
-                    """;
-            System.out.println(menu);
-            opcao = sc.nextInt();
-            sc.nextLine();
+            String menuTexto = """
+            
+            +------------------------------------+
+            |        ESCOLHA A SUA OPÇÃO         |
+            +------------------------------------+
+            | 1 - Buscar livro pelo título       |
+            | 2 - Listar livros registrados      |
+            | 3 - Listar autores registrados     |
+            | 4 - Listar autores vivos em um ano |
+            | 5 - Listar livros por idioma       |
+            | 0 - Sair                           |
+            +------------------------------------+
+            
+            """; // Usando nome 'menuTexto' para evitar conflito com 'menu()'
+            System.out.println(menuTexto);
 
-            switch (opcao) {
-                case 1:
-                    buscarLivroPorTitulo();
-                    break;
-                case 2:
-                    listarLivrosRegistrados();
-                    break;
-                case 3:
-                    listarAutoresRegistrados();
-                    break;
-                case 4:
-                    break;
-                case 5:
-                    break;
-                case 0:
-                    System.out.println("Saindo...");
-                    break;
-                default:
-                    System.out.println("Opção inválida");
+            try {
+                opcao = sc.nextInt();
+                sc.nextLine(); // Consome o '\n' restante após nextInt()
+
+                switch (opcao) {
+                    case 1:
+                        buscarLivroPorTitulo();
+                        break;
+                    case 2:
+                        listarLivrosRegistrados();
+                        break;
+                    case 3:
+                        listarAutoresRegistrados();
+                        break;
+                    case 4:
+                        listarAutoresVivos();
+                        break;
+                    case 5:
+                        listarLivrosPorIdioma();
+                        break;
+                    case 0:
+                        System.out.println("Saindo do LiterAlura. Até mais!");
+                        break;
+                    default:
+                        System.out.println("Opção inválida. Por favor, digite um número entre 0 e 5.");
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Entrada inválida! Por favor, digite um número inteiro.");
+                sc.nextLine(); // Limpa o buffer do scanner para evitar loop infinito
+                opcao = -1; // Garante que o loop continue
+            }
         }
     }
-}
+
+    private void listarLivrosPorIdioma() {
+        System.out.println("""
+                Insira o idioma para realizar a busca:
+                es - espanhol
+                en - inglês
+                fr - francês 
+                pt - português
+                """);
+        var idiomaBusca = sc.nextLine();
+        List<Livro> livrosEncontrados = livroRepository.findByIdioma(idiomaBusca);
+        if (!livrosEncontrados.isEmpty()){
+            System.out.println("\n--- Livros encontrados no idioma '" + idiomaBusca + "' ---");
+            livrosEncontrados.forEach(System.out::println);
+        } else {
+            System.out.println("Nenhum livro encontrado no idioma '" + idiomaBusca + "'.");
+        }
+    }
+
+    private void listarAutoresVivos() {
+        System.out.println("Busque autores vivos no ano de? ");
+        var anoBusca = sc.nextInt();
+        Optional<List<Autor>> autoresVivo = autorRepository.listarAutoresVivos(anoBusca);
+        if (autoresVivo.isPresent()){
+            System.out.println("\nAutores vivos: ");
+            autoresVivo.get().forEach(System.out::println);
+        } else {
+            System.out.println("Não foi encontrado no banco nenhum autor vivo em " + anoBusca);
+        }
+    }
 
     private void listarLivrosRegistrados() {
         var livros = livroRepository.findAll();
         livros.forEach(System.out::println);
+
     }
 
     private void listarAutoresRegistrados() {
@@ -81,16 +126,61 @@ public class Principal {
         System.out.println("Insira o nome do livro que você deseja procurar:");
         var nomeLivro = sc.nextLine();
 
-        var json = consumoAPI.obterDados(ENDERECO + nomeLivro.replace(" ", "%20").trim());
-        DadosLivros dados = converteDados.obterDados(json, DadosLivros.class);
+        Optional<Livro> livroExistenteNoBd = livroRepository.findByTituloContainingIgnoreCase(nomeLivro);
 
-        Optional<DadosLivro> livroOptional = dados.livros().stream().findFirst();
-        Optional<DadosAutor> autorOptional = dados.livros().stream().findFirst().map(l -> l.autor().getFirst());
-        if (livroOptional.isPresent() && autorOptional.isPresent()){
-            Livro livro = new Livro(livroOptional.get());
-            Autor autor = new Autor(autorOptional.get());
-            livro.setAutor(autor);
+        if (livroExistenteNoBd.isPresent()) {
+            System.out.println("Livro já cadastrado no banco de dados:");
+            System.out.println(livroExistenteNoBd.get());
+        } else {
+
+            String enderecoBusca = ENDERECO + nomeLivro.replace(" ", "%20").trim();
+            String jsonResposta = consumoAPI.obterDados(enderecoBusca);
+
+
+            if (jsonResposta == null || jsonResposta.isEmpty() || jsonResposta.equals("[]") || jsonResposta.equals("{}")) {
+                System.out.println("Nenhum resultado encontrado na API para o livro: " + nomeLivro);
+                return; // Encerra o método se não houver dados
+            }
+
+
+            DadosLivros dadosCompletos = converteDados.obterDados(jsonResposta, DadosLivros.class);
+
+
+            Optional<DadosLivro> dadosPrimeiroLivroOptional = dadosCompletos.livros().stream().findFirst();
+
+            if (dadosPrimeiroLivroOptional.isPresent()) {
+                DadosLivro dadosPrimeiroLivro = dadosPrimeiroLivroOptional.get();
+
+
+                Optional<DadosAutor> dadosPrimeiroAutorOptional = dadosPrimeiroLivro.autor().stream().findFirst(); // Assumindo 'autor()' retorna uma List<DadosAutor>
+
+                if (dadosPrimeiroAutorOptional.isPresent()) {
+                    DadosAutor dadosPrimeiroAutor = dadosPrimeiroAutorOptional.get();
+
+
+                    Optional<Autor> autorExistenteNoBd = autorRepository.findBynome(dadosPrimeiroAutor.nome());
+
+                    Autor autorParaSalvar;
+                    if (autorExistenteNoBd.isPresent()) {
+                        autorParaSalvar = autorExistenteNoBd.get();
+                        System.out.println("Autor já cadastrado: " + autorParaSalvar.getNome());
+                    } else {
+                        autorParaSalvar = new Autor(dadosPrimeiroAutor); // Criar novo Autor se não existir
+                        autorRepository.save(autorParaSalvar); // Salvar o novo autor
+                        System.out.println("Novo autor cadastrado: " + autorParaSalvar.getNome());
+                    }
+
+                    Livro novoLivro = new Livro(dadosPrimeiroLivro, autorParaSalvar);
+                    livroRepository.save(novoLivro);
+                    System.out.println("Livro encontrado e cadastrado:");
+                    System.out.println(novoLivro);
+
+                } else {
+                    System.out.println("O primeiro livro retornado pela API não possui informações de autor.");
+                }
+            } else {
+                System.out.println("A busca na API não retornou livros válidos para o título: " + nomeLivro);
+            }
         }
-
     }
-    }
+}
