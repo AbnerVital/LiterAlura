d815310b3585880a532fe652b28f224795fc9256
Abner Vital <avgchaves@gmail.com>
2025-06-25 11:44:29 -0300

    feat: adiciona queries para busca por idioma e por título ignorando case

--- src/main/java/br/com/alura/LiterAlura/repository/LivroRepository.java
+++ src/main/java/br/com/alura/LiterAlura/repository/LivroRepository.java
@@ -2,6 +2,14 @@ package br.com.alura.LiterAlura.repository;
 
 import br.com.alura.LiterAlura.model.Livro;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface LivroRepository extends JpaRepository<Livro, Long> {
+    Optional<Livro> findByTituloContainingIgnoreCase(String nomeLivro);
+
+    @Query("SELECT l FROM Livro l WHERE l.idioma LIKE %:idioma%")
+    List<Livro> findByIdioma(String idioma);
 }
